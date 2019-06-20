3f9640dcbfab2fbe90a15cd39ee7593e1112f990
rlucente <rlucente@redhat.com>
2019-06-20 16:38:53 -0400

    update to 3.11

--- upgrade-server.sh
+++ upgrade-server.sh
@@ -8,6 +8,6 @@ yum -y update openshift-ansible
 envsubst < inventory.orig > inventory.ini
 
 oc login -u ${ADMIN_USER} -p ${ADMIN_PASS}
-ansible-playbook -i inventory.ini /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/upgrades/v3_10/upgrade.yml
+ansible-playbook -i inventory.ini /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/upgrades/v3_11/upgrade.yml
 oc logout
 
